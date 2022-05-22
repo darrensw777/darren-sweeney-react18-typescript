dd6188f09e15090d6b0b7e777c11b79597263f1f
Darren Sweeney <studiotime@hotmail.co.uk>
2022-05-22 10:10:58 +0100

    adding tests

--- src/__tests__/containers/Home.test.tsx
+++ src/__tests__/containers/Home.test.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { Home } from "containers";
 
 test("renders learn react link", () => {
-    render(<App />);
+    render(<Home />);
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
-});
-
-describe("Addition", () => {
-    it("knows that 2 and 2 make 4", () => {
-        expect(2 + 2).toBe(4);
-    });
 });
