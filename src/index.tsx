46625e8205c144066c4033ec8a54db88a23c709d
Darren Sweeney <studiotime@hotmail.co.uk>
2022-05-22 18:39:26 +0100

    code tidy

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,9 @@
-import { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes/';
 import { I18nContext, i18n } from 'utils/context';
 import './styles/styles.scss';
-
-// interface I18nInterface {
-//   key: string;
-//   default: DefaultKeyInterface;
-// }
-
-// interface DefaultKeyInterface {
-//   language: string;
-//   locale: string;
-// }
-
-// const i18n: I18nInterface = {
-//     key: 'i18n',
-//     default: {
-//         language: 'en',
-//         locale: 'en-GB',
-//     },
-// };
-
-// export const I18nContext = createContext<I18nInterface | null>(null);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(
