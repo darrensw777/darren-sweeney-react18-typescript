9d620cae02b4efa6dcd4179b2a31fe0da0cdbe83
Darren Sweeney <studiotime@hotmail.co.uk>
2022-06-03 14:21:52 +0100

    removed social share

--- src/components/SocialShare/SocialShare.tsx
+++ src/components/SocialShare/SocialShare.tsx
@@ -1,38 +1,39 @@
-import {
-    EmailShareButton,
-    EmailIcon,
-    FacebookShareButton,
-    FacebookIcon,
-    TwitterShareButton,
-    TwitterIcon,
-    WhatsappShareButton,
-    WhatsappIcon,
-} from 'react-share';
+// import {
+//     EmailShareButton,
+//     EmailIcon,
+//     FacebookShareButton,
+//     FacebookIcon,
+//     TwitterShareButton,
+//     TwitterIcon,
+//     WhatsappShareButton,
+//     WhatsappIcon,
+// } from 'react-share';
 
-const SocialShare = () => {
-    const shareUrl = document.URL;
-    const title = document.title;
-    return (
-        <div className="social__outer">
-            <div className="inner-content">
-                <h4>Share this article</h4>
-                <div className="social__container">
-                    <EmailShareButton url={shareUrl} subject={title} className="Demo__some-network__share-button">
-                        <EmailIcon size={32} round />
-                    </EmailShareButton>
-                    <FacebookShareButton url={shareUrl} quote={title} className="Demo__some-network__share-button">
-                        <FacebookIcon size={32} round />
-                    </FacebookShareButton>
-                    <TwitterShareButton url={shareUrl} title={title} className="Demo__some-network__share-button">
-                        <TwitterIcon size={32} round />
-                    </TwitterShareButton>
-                    <WhatsappShareButton url={shareUrl} title={title} className="Demo__some-network__share-button">
-                        <WhatsappIcon size={32} round />
-                    </WhatsappShareButton>
-                </div>
-            </div>
-        </div>
-    );
-};
+// const SocialShare = () => {
+//     const shareUrl = document.URL;
+//     const title = document.title;
+//     return (
+//         <div className="social__outer">
+//             <div className="inner-content">
+//                 <h4>Share this article</h4>
+//                 <div className="social__container">
+//                     <EmailShareButton url={shareUrl} subject={title} className="Demo__some-network__share-button">
+//                         <EmailIcon size={32} round />
+//                     </EmailShareButton>
+//                     <FacebookShareButton url={shareUrl} quote={title} className="Demo__some-network__share-button">
+//                         <FacebookIcon size={32} round />
+//                     </FacebookShareButton>
+//                     <TwitterShareButton url={shareUrl} title={title} className="Demo__some-network__share-button">
+//                         <TwitterIcon size={32} round />
+//                     </TwitterShareButton>
+//                     <WhatsappShareButton url={shareUrl} title={title} className="Demo__some-network__share-button">
+//                         <WhatsappIcon size={32} round />
+//                     </WhatsappShareButton>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
 
-export default SocialShare;
+// export default SocialShare;
+export {};
